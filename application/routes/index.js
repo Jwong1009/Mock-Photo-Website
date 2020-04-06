9c3509d356debd726b80b60d8ccec8e2ac75ad7c
Joanne Wong <jwong45@mail.sfsu.edu>
2020-04-05 22:23:15 -0700

    adds all html and corresponding css files

--- application/routes/index.js
+++ application/routes/index.js
@@ -5,5 +5,23 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.sendFile('index.html');
 });
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.sendFile('login.html', {root: 'public/'});
+});
+
+/* GET registration page. */
+router.get('/register', function(req, res, next) {
+  res.sendFile('registration.html', {root: 'public/'});
+});
+
+/* GET uploading image page. */
+router.get('/upload', function(req, res, next) {
+  res.sendFile('postimage.html', {root: 'public/'});
+});
+/* GET single page. */
+router.get('/photo', function(req, res, next) {
+  res.sendFile('singleimg.html', {root: 'public/'});
+});
 
 module.exports = router;
